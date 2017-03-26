71c91ffc2eb058ff7bef464a0bf72053740e7f2d
vagrant <vagrant@homestead>
2017-03-26 21:35:15 +0000

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yechefFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yechefFrontApp
+ */
+angular.module('yechefFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yechefFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yechefFrontApp
+ */
+angular.module('yechefFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

6db67db06506d67c7c90aafe63f9b03792ca8a10
vagrant <vagrant@homestead>
2017-03-26 21:47:01 +0000

    social login module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,28 @@
  * Main module of the application.
  */
 angular
-  .module('yechefFrontApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.module('yechefFrontApp', [
+		'ngAnimate',
+		'ngCookies',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch',
+		'satellizer',
+	])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl',
+				controllerAs: 'main'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutCtrl',
+				controllerAs: 'about'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});

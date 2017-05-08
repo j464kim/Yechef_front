71e3cc90cd798dd65c42b654e3f9a7059d3067e1
vagrant <vagrant@homestead>
2017-05-08 12:07:12 +0000

    [WIP] Dish CRUD skeleton created. TODO: need to be implemented

---
+++ app/dishes/show/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+angular.module('dish.show', [
+    'dishes.api',
+    'directive.loader'
+])
+
+    .controller('DishShowController', ['$state', 'DishesAPI',
+        function($state, DishesAPI){
+
+            /*********************
+             *	Private Variables
+             **********************/
+                // reference to this controller
+            var that = this;
+
+            /*********************
+             *	Public Variables
+             **********************/
+            this.totalItems = 0;
+            this.currentPage = 0;
+
+            /*********************
+             *	Private Functions
+             **********************/
+
+            function _init() {
+                _getDishes();
+            }
+
+            function _getDishes() {
+                var pageNum = that.currentPage || that.currentPage++;
+
+                DishesAPI.list(pageNum).then(function(response){
+                    console.log(response);
+                    that.dishes = response.data;
+                    that.totalItems = response.total;
+                    that.currentPage = response.current_page;
+                }, function(response) {
+                    // TODO handle error state
+                    console.error(response);
+                });
+            }
+
+            /*********************
+             *	Public Functions
+             **********************/
+            this.getDishes = _getDishes;
+
+
+            /*********************
+             *	Initialization
+             **********************/
+            _init();
+
+            /*********************
+             *	EVENTS
+             **********************/
+
+
+
+        }
+    ])/**
+ * Created by ghkdt on 2017-05-06.
+ */
--- app/scripts/route.js
+++ app/scripts/route.js
@@ -3,6 +3,10 @@
 angular.module('routes', [
 	'dish.list',
 	'dish.list.infinite',
+	'dish.show',
+	'dish.create',
+	'dish.update',
+	'dish.destroy',
 ])
 
 .config(function ($stateProvider, $urlRouterProvider) {
@@ -26,7 +30,27 @@ angular.module('routes', [
 			url: 'list/infinite',
 			templateUrl: 'dishes/list-infinite/template.html',
 			controller: 'DishListInfiniteController as DishCtrl',
-		});
+		})
+        .state('dish.show', {
+            url: '/show',
+            templateUrl: 'dishes/show/template.html',
+            controller: 'DishShowController as DishCtrl',
+        })
+        .state('dish.create', {
+            url: '/create',
+            templateUrl: 'dishes/create/template.html',
+            controller: 'DishCreateController as DishCtrl',
+        })
+        .state('dish.update', {
+            url: '/update',
+            templateUrl: 'dishes/update/template.html',
+            controller: 'DishUpdateController as DishCtrl',
+        })
+        .state('dish.destroy', {
+            url: '/destroy',
+            templateUrl: 'dishes/destroy/template.html',
+            controller: 'DishDestroyController as DishCtrl',
+        });
 
 	// For any unmatched url, redirect to /
 	$urlRouterProvider.otherwise('/');

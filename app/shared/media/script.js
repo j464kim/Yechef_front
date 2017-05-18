e107295ee8144257c60d0efce70612e906c0e260
JunGwon <j464kim@uwaterloo.ca>
2017-05-18 12:56:06 -0400

    Implement media upload to Kitchen

--- app/shared/media/script.js
+++ app/shared/media/script.js
@@ -25,13 +25,38 @@ angular.module('mediaUpload', [
 
 			$scope.dropzoneConfig = {
 
-				'options': { // passed into the Dropzone constructor
-					'url': '//laravel.dev/api/media',
-					'method': 'POST'
+				options: { // passed into the Dropzone constructor
+					url: '//laravel.dev/api/media',
+					method: 'POST',
+					maxFileSize: 4, // 4mb
+					acceptedFiles: 'image/jpeg, images/jpg, image/png',
+					parallelUploads: 10,
+					uploadMultiple: true,
+					autoProcessQueue: false,
+					addRemoveLinks: true,
+
+					init: function () {
+						var submitButton = document.querySelector("#submit-media");
+						myDropzone = this; // closure
+
+						submitButton.addEventListener("click", function () {
+							myDropzone.processQueue(); // Tell Dropzone to process all queued files.
+						});
+
+						myDropzone.on("complete", function (file) {
+							// alert("Image is uploaded.");
+							myDropzone.removeFile(file);
+						});
+					}
 				},
 
 				'eventHandlers': {
 					'sending': function (file, xhr, formData) {
+						var email = $("#email").val();
+						console.log('email is: ' + email);
+						console.log('//laravel.dev/api/media/' + email);
+						// pass email to request
+						formData.append('email', email);
 						console.log('image is being processed');
 					},
 					'success': function (file, response) {

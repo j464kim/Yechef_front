3e61cbaeb0bebfc0fb05e6d84cf7ec045c74fc93
JunGwon <j464kim@uwaterloo.ca>
2017-06-10 19:53:34 -0400

    Finished My Kitchen

--- app/components/users/kitchen/userKitchenRoute.js
+++ app/components/users/kitchen/userKitchenRoute.js
@@ -2,6 +2,7 @@
 
 angular.module('userKitchen', [
 	'user',
+	'user.kitchen',
 ])
 
 	.config(function ($stateProvider) {
@@ -14,25 +15,68 @@ angular.module('userKitchen', [
 			.state('user.kitchen.dashboard', {
 				url: '/dasborad',
 				templateUrl: 'components/users/kitchen/dashboard/kitchenDashboard.html',
+				controller: 'userKitchenController as userKitchenCtrl',
 			})
 			.state('user.kitchen.general', {
 				url: '/general',
+				abstract: true,
+				template: '<ui-view/>'
+			})
+			.state('user.kitchen.general.view', {
+				url: '/view',
 				templateUrl: 'components/users/kitchen/general/kitchenGeneral.html',
+				controller: 'userKitchenController as userKitchenCtrl',
+			})
+			.state('user.kitchen.general.edit', {
+				url: '/myKitchen/edit',
+				templateUrl: 'components/users/kitchen/general/myKitchenEdit.html',
+				controller: 'userKitchenController as userKitchenCtrl',
 			})
 			.state('user.kitchen.dish', {
 				url: '/dish',
 				templateUrl: 'components/users/kitchen/dish/kitchenDish.html',
+				controller: 'userKitchenController as userKitchenCtrl',
 			})
 			.state('user.kitchen.order', {
 				url: '/order',
 				templateUrl: 'components/users/kitchen/order/kitchenOrder.html',
+				controller: 'userKitchenController as userKitchenCtrl',
 			})
 			.state('user.kitchen.subscriber', {
 				url: '/subscriber',
 				templateUrl: 'components/users/kitchen/subscriber/kitchenSubscriber.html',
+				controller: 'userKitchenController as userKitchenCtrl',
 			})
 			.state('user.kitchen.payout', {
 				url: '/payout',
+				abstract: true,
+				template: '<ui-view/>'
+			})
+			.state('user.kitchen.payout.view', {
+				url: '/view',
 				templateUrl: 'components/users/kitchen/payout/kitchenPayout.html',
+				controller: 'userKitchenController as userKitchenCtrl',
+			})
+			.state('user.kitchen.payout.preference', {
+				url: '/view',
+				templateUrl: 'components/users/kitchen/payout/payoutPreference.html',
+				controller: 'userKitchenController as userKitchenCtrl',
+			})
+			.state('user.kitchen.payout.setting', {
+				url: '/setting',
+				abstract: true,
+				templateUrl: 'components/users/kitchen/payout/payoutSetting.html',
+				controller: 'userKitchenController as userKitchenCtrl',
+			})
+			.state('user.kitchen.payout.setting.view', {
+				url:'',
+				views: {
+					'paypal': {
+						templateUrl: 'components/users/kitchen/payout/addPayPal.html',
+					},
+					'bankAccount': {
+						templateUrl: 'components/users/kitchen/payout/addBankAccount.html',
+					},
+				}
 			})
 	});
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -53,6 +53,8 @@ module.exports = function(config) {
       'bower_components/google-maps-utility-library-v3-keydragzoom/dist/keydragzoom.js',
       'bower_components/js-rich-marker/src/richmarker.js',
       'bower_components/angular-google-maps/dist/angular-google-maps.js',
+      'bower_components/chart.js/dist/Chart.js',
+      'bower_components/angular-chart.js/dist/angular-chart.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',

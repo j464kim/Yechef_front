abfda65a8cbb52b9ead10281856b974100756845
sangmooonhwang <ghkdtkdans@gmail.com>
2017-06-16 19:48:48 +0000

    Show auth error message in toast

--- app/components/users/register/script.js
+++ app/components/users/register/script.js
@@ -6,8 +6,8 @@ angular.module('user.register', [
 ])
 
 
-	.controller('UserRegisterController', ['$state', 'AuthAPI', 'devHelper',
-		function ($state, AuthAPI, devHelper) {
+	.controller('UserRegisterController', ['$state', 'AuthAPI', 'devHelper', '$mdToast',
+		function ($state, AuthAPI, devHelper, $mdToast) {
 
 			/*********************
 			 *    Private Variables
@@ -41,6 +41,20 @@ angular.module('user.register', [
 							devHelper.log(response);
 						},
 						function (response) {
+							var message;
+							if (response.data.message.includes('already')) {
+								message = 'The email is already taken';
+							} else {
+								message = response.data.message;
+							}
+							$mdToast.show(
+								$mdToast.simple()
+									.textContent(message)
+									.position('top center')
+									.highlightClass('md-warn')
+									.capsule(true)
+									.hideDelay(3000)
+							);
 							console.error(response);
 						}
 					);

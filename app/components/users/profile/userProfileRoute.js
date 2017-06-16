f4bdf2be63c45e3a0e8bdac64d8032d7caea997c
JunGwon <j464kim@uwaterloo.ca>
2017-06-15 23:17:29 -0400

    refactor user profile directory structure

--- app/components/users/profile/userProfileRoute.js
+++ app/components/users/profile/userProfileRoute.js
@@ -14,7 +14,7 @@ angular.module('profile', [
 			})
 			.state('user.profile.main', {
 				url: '/main',
-				templateUrl: 'components/users/profile/partial/userMain.html',
+				templateUrl: 'components/users/profile/main/userMain.html',
 			})
 			.state('user.profile.info', {
 				url: '/info',
@@ -23,32 +23,32 @@ angular.module('profile', [
 			})
 			.state('user.profile.info.view', {
 				url: '/view',
-				templateUrl: 'components/users/profile/partial/userInfoView.html',
+				templateUrl: 'components/users/profile/general/userInfoView.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.info.edit', {
 				url: '/edit',
-				templateUrl: 'components/users/profile/partial/userInfoEdit.html',
+				templateUrl: 'components/users/profile/general/userInfoEdit.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.order', {
 				url: '/myOrder',
-				templateUrl: 'components/users/profile/partial/userOrder.html',
+				templateUrl: 'components/users/profile/order/userOrder.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.forked', {
 				url: '/forked',
-				templateUrl: 'components/users/profile/partial/userFork.html',
+				templateUrl: 'components/users/profile/fork/userFork.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.subscribe', {
 				url: '/subscribe',
-				templateUrl: 'components/users/profile/partial/userSubscribe.html',
+				templateUrl: 'components/users/profile/subscription/userSubscribe.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.privacySetting', {
 				url: '/privacySetting',
-				templateUrl: 'components/users/profile/partial/userPrivacy.html',
+				templateUrl: 'components/users/profile/privacy/userPrivacy.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.payment', {
@@ -58,17 +58,17 @@ angular.module('profile', [
 			})
 			.state('user.profile.payment.list', {
 				url: '/list',
-				templateUrl: 'components/users/profile/partial/userPaymentList.html',
+				templateUrl: 'components/users/profile/payment/userPaymentList.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.payment.create', {
 				url: '/create',
-				templateUrl: 'components/users/profile/partial/userPaymentCreate.html',
+				templateUrl: 'components/users/profile/payment/userPaymentCreate.html',
 				controller: 'userMainController as userMainCtrl',
 			})
 			.state('user.profile.payment.edit', {
 				url: '/edit',
-				templateUrl: 'components/users/profile/partial/userPaymentEdit.html',
+				templateUrl: 'components/users/profile/payment/userPaymentEdit.html',
 				controller: 'userMainController as userMainCtrl',
 			});
 	});

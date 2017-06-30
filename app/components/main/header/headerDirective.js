c3fabd3efe4573f3e659a3add238881dc7415834
JunGwon <j464kim@uwaterloo.ca>
2017-06-30 00:56:41 -0400

    remove unnecessary code

--- app/components/main/header/headerDirective.js
+++ app/components/main/header/headerDirective.js
@@ -10,7 +10,6 @@ module.directive('pageHeader', ['$rootScope', function () {
 		templateUrl: "components/main/header/header.html",
 		controller: ['$scope', '$filter', '$rootScope', function ($scope, $filter, $rootScope) {
 			// Your behaviour goes here :)
-			$scope.currentUser = $rootScope.currentUser;
 		}]
 	}
 }]);

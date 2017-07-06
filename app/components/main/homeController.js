b5e273bdc417a11ac07e39a71a1c26c74443efba
sangmooonhwang <ghkdtkdans@gmail.com>
2017-07-06 12:38:48 +0000

    Fix a bug

--- app/components/main/homeController.js
+++ app/components/main/homeController.js
@@ -48,7 +48,6 @@ angular.module('home', [])
 		self.querySearch = querySearch;
 		self.selectedItemChange = selectedItemChange;
 		self.searchTextChange = searchTextChange;
-		self.distance = 0;
 
 		self.nationality = newNationality;
 

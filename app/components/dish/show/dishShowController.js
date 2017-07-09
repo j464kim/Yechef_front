df46ee07d9381ddf159871c9b637fce0dad3b03d
sangmooonhwang <ghkdtkdans@gmail.com>
2017-07-09 23:54:59 +0000

    Add Gmap to Dish Show page

--- app/components/dish/show/dishShowController.js
+++ app/components/dish/show/dishShowController.js
@@ -1,61 +1,85 @@
 'use strict';
 
 angular.module('dish.show', [
-    'share',
-    'carousel',
-    'dishes.api',
+	'share',
+	'carousel',
+	'dishes.api',
 ])
 
-    .controller('DishShowController', ['$state', '$stateParams', 'DishesAPI', 'KitchenAPI',
-        function ($state, $stateParams, DishesAPI, KitchenAPI) {
+	.controller('DishShowController', ['$state', '$stateParams', 'DishesAPI', 'KitchenAPI', 'devHelper', 'MapAPI',
+		function ($state, $stateParams, DishesAPI, KitchenAPI, devHelper, MapAPI) {
 
-            /*********************
-             *    Private Variables
-             **********************/
-                // reference to this controller
-            var that = this;
+			/*********************
+			 *    Private Variables
+			 **********************/
+				// reference to this controller
+			var that = this;
 
-            /*********************
-             *    Public Variables
-             **********************/
-            this.dishId = $stateParams.id;
+			/*********************
+			 *    Public Variables
+			 **********************/
+			this.dishId = $stateParams.id;
+			this.map = MapAPI.getMapOption();
+			this.map.options.gestureHandling = 'none';
+			this.map.zoom = 20;
+			this.mapCtrl = {};
+			this.marker = {id: 0};
 
-            /*********************
-             *    Private Functions
-             **********************/
+			/*********************
+			 *    Private Functions
+			 **********************/
 
-            function _init() {
-                _showDish();
-            }
+			function _init() {
+				_showDish();
 
-            function _showDish() {
-                DishesAPI.show(that.dishId)
-                    .then(function (response) {
-                        that.dish = response;
-                        _getKitchen(that.dish.kitchen_id);
-                    }, function (response) {
-                        // TODO handle error state
-                        console.error(response);
-                    });
+			}
 
-            }
+			function _showDish() {
+				DishesAPI.show(that.dishId)
+					.then(function (response) {
+						devHelper.log(response);
+						that.dish = response;
+						_getKitchen(that.dish.kitchen_id);
+					}, function (response) {
+						// TODO handle error state
+						console.error(response);
+					});
 
-            function _getKitchen(kitchenId) {
-                KitchenAPI.show(kitchenId).then(function (response) {
-                    that.kitchen = response;
-                }, function (response) {
-                    // TODO handle error state
-                    console.error(response);
-                });
-            }
+			}
 
-            /*********************
-             *    Initialization
-             **********************/
-            _init();
+			function _getKitchen(kitchenId) {
+				KitchenAPI.show(kitchenId).then(function (response) {
+					devHelper.log(response);
+					that.kitchen = response;
+					_locateKitchen(that.kitchen.address);
+				}, function (response) {
+					// TODO handle error state
+					console.error(response);
+				});
+			}
 
-            /*********************
-             *    EVENTS
-             **********************/
-        }
-    ]);
+			function _locateKitchen(address) {
+				MapAPI.geocode(address).then(function (result) {
+						if (result) {
+							that.map.center.latitude = result[0].geometry.location.lat();
+							that.map.center.longitude = result[0].geometry.location.lng();
+							that.marker.coords = {
+								latitude: result[0].geometry.location.lat(),
+								longitude: result[0].geometry.location.lng()
+							};
+							that.mapCtrl.refresh();
+						}
+					}
+				);
+			}
+
+			/*********************
+			 *    Initialization
+			 **********************/
+			_init();
+
+			/*********************
+			 *    EVENTS
+			 **********************/
+		}
+	]);

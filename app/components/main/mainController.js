d124381702d3f5ba8e46d3343df58a0ff5c03682
jason <jasonheshuai@gmail.com>
2017-07-09 01:53:36 +0000

    ng material follow less color scheme config now

--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -2,8 +2,8 @@
 
 angular.module('main', [])
 
-	.controller('MainController', ['$scope', '$rootScope', 'AuthAPI', 'devHelper', '$state', 'sessionService', '$mdTheming', 'themeProvider',
-		function ($scope, $rootScope, AuthAPI, devHelper, $state, sessionService, $mdTheming, themeProvider) {
+	.controller('MainController', ['$scope', '$rootScope', 'AuthAPI', 'devHelper', '$state', 'sessionService', '$mdTheming', 'themeProvider', 'genericService',
+		function ($scope, $rootScope, AuthAPI, devHelper, $state, sessionService, $mdTheming, themeProvider, genericService) {
 
 			var that = this;
 
@@ -38,36 +38,26 @@ angular.module('main', [])
 
 			// themeing
 			$scope.$watch(angular.bind(this, function () {
-				return this.color;
+				return this.themes;
 			}), function (value) {
 				if(value) {
-					console.log(value);
-					themeProvider.definePalette('amazingPaletteName', {
-						'50': 'ffebee',
-						'100': 'ffcdd2',
-						'200': 'ef9a9a',
-						'300': 'e57373',
-						'400': 'ef5350',
-						'500': 'f44336',
-						'600': 'e53935',
-						'700': 'd32f2f',
-						'800': 'c62828',
-						'900': 'b71c1c',
-						'A100': 'ff8a80',
-						'A200': 'ff5252',
-						'A400': 'ff1744',
-						'A700': 'd50000',
-						'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
-						                                	// on this palette should be dark or light
+					if(value.primary) {
+						themeProvider.definePalette('primaryTheme', value.primary);
+					}
 
-						'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-						'200', '300', '400', 'A100'],
-						'contrastLightColors': undefined    // could also specify this if default was 'dark'
-					});
+					if(value.secondary) {
+						themeProvider.definePalette('secondaryTheme', value.secondary);
+					}
 
-					themeProvider.theme('default').primaryPalette('amazingPaletteName').accentPalette('green');
+					if(value.ternary) {
+						themeProvider.definePalette('ternaryTheme', value.ternary);
+					}
+
+					themeProvider.theme('default')
+						.primaryPalette('primaryTheme')
+						.accentPalette('secondaryTheme')
+						.warnPalette('ternaryTheme');
 					$mdTheming.generateTheme('default');
-					themeProvider.setDefaultTheme('default');
 				}
 			});
 		}
--- app/shared/themePixel/themePixelDirective.js
+++ app/shared/themePixel/themePixelDirective.js
@@ -1,28 +1,99 @@
 'use strict';
 
-angular.module('directive.themePixel', [])
-.directive('themePixel', [function () {
-	return {
-		restrict: 'E',
-		replace: true,
-		template: `
-			<div id="themePixel">
-				<div class="bg-primary"></div>
-				<div class="bg-secondary"></div>
-				<div class="bg-ternary"></div>
-				<div class="bg-quaternary"></div>
-			</div>
-		`,
-		scope: {
-			color: "="	
-		},
-		link: function($scope) {
-			$scope.color = {
-				primary: angular.element('.bg-primary').css('background-color'),
-				secondary: angular.element('.bg-secondary').css('background-color'),
-				ternary: angular.element('.bg-ternary').css('background-color'),
-				quaternary: angular.element('.bg-quaternary').css('background-color'),
-			};
-		}
-	};
-}]);
+angular.module('directive.themePixel', [
+	'helper',
+])
+.directive('themePixel', ['genericService', 
+	function (genericService) {
+		return {
+			restrict: 'E',
+			replace: true,
+			template: `
+				<div id="themePixel">
+					<div class="bg-primary"></div>
+					<div class="bg-secondary"></div>
+					<div class="bg-ternary"></div>
+					<div class="bg-quaternary"></div>
+				</div>
+			`,
+			scope: {
+				themes: "="	
+			},
+			link: function($scope) {
+				var primaryElem 	= angular.element('.bg-primary');
+				var secondaryElem 	= angular.element('.bg-secondary');
+				var ternaryElem 	= angular.element('.bg-ternary');
+				var quaternaryElem 	= angular.element('.bg-quaternary');
+
+				var defaultColor 	= 'rgb(255, 255, 0)';
+				var defaultFontColor= 'rgb(255, 255, 255)';
+
+				var primary 		= primaryElem.css('background-color') || defaultColor;
+				var primaryFont 	= primaryElem.css('color') || defaultFontColor;
+				var secondary 		= secondaryElem.css('background-color') || defaultColor;
+				var secondaryFont 	= secondaryElem.css('color') || defaultFontColor;
+				var ternary 		= ternaryElem.css('background-color') || defaultColor;
+				var ternaryFont 	= ternaryElem.css('color') || defaultFontColor;
+				var quaternary		= quaternaryElem.css('background-color') || defaultColor;
+				var quaternaryFont	= quaternaryElem.css('color') || defaultFontColor;
+
+				var primaryTheme = {
+					"50": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, 0.25, true)},
+					"100": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, 0.2, true)},
+					"200": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, 0.15, true)},
+					"300": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, 0.1, true)},
+					"400": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, 0.05, true)},
+					"500": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, 0, true)},
+					"600": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, -0.05, true)},
+					"700": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, -0.1, true)},
+					"800": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, -0.15, true)},
+					"900": {contrast: genericService.parseRgbStr(primaryFont), value: genericService.shadeRGBColor(primary, -0.2, true)},
+					"A100": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.2, true)},
+					"A200": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.1, true)},
+					"A400": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, -0.1, true)},
+					"A700": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, -0.2, true)}
+				};
+
+				var secondaryTheme = {
+					"50": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.25, true)},
+					"100": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.2, true)},
+					"200": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.15, true)},
+					"300": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.1, true)},
+					"400": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0.05, true)},
+					"500": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, 0, true)},
+					"600": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, -0.05, true)},
+					"700": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, -0.1, true)},
+					"800": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, -0.15, true)},
+					"900": {contrast: genericService.parseRgbStr(secondaryFont), value: genericService.shadeRGBColor(secondary, -0.2, true)},
+					"A100": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.2, true)},
+					"A200": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.1, true)},
+					"A400": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, -0.1, true)},
+					"A700": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, -0.2, true)}
+				};
+
+				var ternaryTheme = {
+					"50": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.25, true)},
+					"100": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.2, true)},
+					"200": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.15, true)},
+					"300": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.1, true)},
+					"400": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0.05, true)},
+					"500": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, 0, true)},
+					"600": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, -0.05, true)},
+					"700": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, -0.1, true)},
+					"800": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, -0.15, true)},
+					"900": {contrast: genericService.parseRgbStr(ternaryFont), value: genericService.shadeRGBColor(ternary, -0.2, true)},
+					"A100": {contrast: genericService.parseRgbStr(quaternaryFont), value: genericService.shadeRGBColor(quaternary, 0.2, true)},
+					"A200": {contrast: genericService.parseRgbStr(quaternaryFont), value: genericService.shadeRGBColor(quaternary, 0.1, true)},
+					"A400": {contrast: genericService.parseRgbStr(quaternaryFont), value: genericService.shadeRGBColor(quaternary, -0.1, true)},
+					"A700": {contrast: genericService.parseRgbStr(quaternaryFont), value: genericService.shadeRGBColor(quaternary, -0.2, true)}
+				};
+
+				$scope.themes = {
+					primary: primaryTheme,
+					secondary: secondaryTheme,
+					ternary: ternaryTheme
+				};
+			}
+		};
+	}
+]);

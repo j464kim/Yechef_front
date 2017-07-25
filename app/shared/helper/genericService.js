c93be3ac5a0c062e30e707a184881eb03fb1986c
sangmooonhwang <ghkdtkdans@gmail.com>
2017-07-25 21:38:03 +0000

    Capitalize Model Name when getting ModelType from generic Service

--- app/shared/helper/genericService.js
+++ app/shared/helper/genericService.js
@@ -12,7 +12,7 @@ angular.module('helper', [])
 		return {
 			getModelType: function ($state) {
 				var stateName = $state.current.name;
-				var modelName = stateName.split(".")[0];
+				var modelName = _.capitalize(stateName.split(".")[0]);
 				var modelType = 'App\\Models\\' + modelName;
 
 				var modelInfo = {
